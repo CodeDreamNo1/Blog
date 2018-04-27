64631bd8459d4be9d6a002d7a2892eab4f286a3c
gys <nova_gaoys@126.com>
2018-04-27 18:03:52 +0800

    提交代码

---
+++ src/client/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// 导入对应的vuex文件
+import store from './../store/store'
+
+// 导入相应的子组件
+import index from './../components/index'
+
+Vue.use(Router)
+
+var router = new Router({
+  mode: 'history',
+  routes: [
+    { name: 'index', path: '/', component: index },
+  ]
+})
+
+export default router
